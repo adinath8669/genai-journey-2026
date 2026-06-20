67ac60cbefe806da2b34465ddca6daf86c3fdc44
DESKTOP-46B0AUG\ADINATH <kadamadinath000@gmail.com>
2026-06-19 23:26:46 -0500

    day 1 commit

---
+++ 02_LLM_Fundamentals/Week-01-LLM-Fundamentals/Assignments/llm_basics.py
@@ -0,0 +1,11 @@
+sentence = "I want to become a Generative AI Engineer."
+
+tokens = sentence.split()
+
+print("Sentence:", sentence)
+print("Tokens:")
+
+for token in tokens:
+    print(token)
+
+print("\nNumber of Tokens:", len(tokens))
